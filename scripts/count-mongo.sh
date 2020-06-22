4588b5ddb1eaab5f6b51a52e58825c23aca04e57
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2020-06-22 16:52:44 +0200

    Fix counting Mongo instances on Mac OS X
    Work around missing pgrep

--- scripts/count-mongo.sh
+++ scripts/count-mongo.sh
@@ -3,8 +3,11 @@
 INTERVAL=$1
 COUNT=$2
 
+COMMAND="pgrep --count --full mongod"
+[[ ! -f $(which pgrep) ]] && COMMAND="ps aux | grep mongod | grep --invert-match grep | wc -l"
+
 for i in $(seq $COUNT); do
     echo -e -n $(date '+%H:%M:%S') '\t'
-    echo $(pgrep --count --full mongod)
+    eval $COMMAND
     sleep $INTERVAL
 done
